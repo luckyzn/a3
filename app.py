fe4fefd74523cd39d597f24de3de801bca02a172
luckyzn <43798356+luckyzn@users.noreply.github.com>
2023-11-27 21:57:07 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,46 +3,73 @@ from sklearn.linear_model import LinearRegression as lr
 import pandas as pd
 import numpy as np
 
+dicionario_doencas = {
+   1: "DRC",
+   2: "Hipercolesterolemia",
+   3:"Anemia",
+   4:"Lesao Hepatica",
+   5:"Infeccao Bacteriana",
+   6:"Desidratação",
+   7:"Infecccao Parasitaria",
+   8:"Infeccao Viral",
+   9:"DRC e Cardiopatia",
+   10:"Neoplasia Hepatica",
+   11:"Anemia Hemolitica",
+   12:"Diabetes",
+   13:"Pre-Diabetes",
+   14:"Anemia e Infeccao",
+   15:"Hepatopata",
+   16:"Trombocitopenia e Inflamacao",
+   17:"Hipoblasia Mieloide",
+   18:"Pancreatite",
+   19:"Inflamacao Grave"
+}
 diagnostico = ["ID"]
-colunas = ['eritrocitos','hemoglobina','hematocrito','hcm','vgm','chgm','metarrubricitos','proteina_plasmatica','leucocitos','leucograma','segmentados','bastonetes','blastos','metamielocitos',
-          'mielocitos','linfocitos','monocitos','eosinofilos','basofilos','plaquetas']
+colunas = ["eritrocitos","hemoglobina","hematocrito","hcm","vgm","chgm","metarrubricitos",
+    "proteina_plasmatica","leucocitos","leucograma","segmentados","bastonetes","blastos","metamielocitos",
+    "mielocitos","linfocitos","monocitos","eosinofilos","basofilos","plaquetas"]
 planilha = pd.read_csv ('dataset-hemograma.csv')
 def cria_modelo():
-  return lr().fit(planilha[colunas],planilha[diagnostico])
+  return lr().fit(planilha[colunas],planilha["ID"])
 
 modelo = cria_modelo()
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST'])
+@app.route("/hemograma", methods=['POST'])
 def hemograma():
-   
-    data = request.json(silent=True)
-    #data = request.get_json(silent=True)
+    print(request)
+    data = request.json
     if data is None:
       return jsonify({"error": "Erro aqui"}), 400
-    num_eritrocitos = data.get('eritrocitos')
-    num_hemoglobina = data.get('hemoglobina')
-    num_hematocrito = data.get ('hematocrito')
-    num_hcm = data.get('hcm')
-    num_vgm = data.get('vgm')
-    num_chgm = data.get ('chgm')
-    num_metarrubricitos = data.get('metarrubricitos')
-    num_proteina = data.get('proteina_plasmatica')
-    num_leucocitos = data.get('leucocitos')
-    num_leucograma = data.get('leucograma')
-    num_segmentados = data.get('segmentados')
-    num_bastonetes = data.get ('bastonetes')
-    num_blastos = data.get ('blastos')
-    num_metamielocitos = data.get('metamielocitos')
-    num_mielocitos = data.get('mielocitos')
-    num_linfocitos = data.get('liefocitos')
-    num_monocitos = data.get('monocitos')
-    num_eosinofilos = data.get('eosinofilos')
-    num_basofilos = data.get('basofilos')
-    num_plaquetas = data.get('plaquetas')
-    dadosFinais = np.array([[float(num_eritrocitos),float(num_hemoglobina),float(num_hematocrito),float(num_hcm),float(num_vgm),float(num_chgm),float(num_metarrubricitos),float(num_proteina),float(num_leucocitos),float(num_leucograma),float(num_segmentados),float(num_bastonetes),float(num_blastos),float(num_metamielocitos),
-                          float(num_mielocitos),float(num_linfocitos),float(num_monocitos),float(num_eosinofilos),float(num_basofilos),float(num_plaquetas)]])
+    num_eritrocitos = data['eritrocitos']
+    num_hemoglobina = data['hemoglobina']
+    num_hematocrito = data['hematocrito']
+    num_hcm = data['hcm']
+    num_vgm = data['vgm']
+    num_chgm = data['chgm']
+    num_metarrubricitos = data['metarrubricitos']
+    num_proteina = data['proteina_plasmatica']
+    num_leucocitos = data['leucocitos']
+    num_leucograma = data['leucograma']
+    num_segmentados = data['segmentados']
+    num_bastonetes = data['bastonetes']
+    num_blastos = data['blastos']
+    num_metamielocitos = data['metamielocitos']
+    num_mielocitos = data['mielocitos']
+    num_linfocitos = data['linfocitos']
+    num_monocitos = data['monocitos']
+    num_eosinofilos = data['eosinofilos']
+    num_basofilos = data['basofilos']
+    num_plaquetas = data['plaquetas']
+
+    dadosFinais = np.array([[num_eritrocitos,num_hemoglobina,num_hematocrito,num_hcm,num_vgm,
+        num_chgm,num_metarrubricitos,num_proteina,num_leucocitos,num_leucograma,num_segmentados,
+        num_bastonetes,num_blastos,num_metamielocitos,
+        num_mielocitos,num_linfocitos,num_monocitos,num_eosinofilos,num_basofilos,num_plaquetas]])
     resultado = modelo.predict(dadosFinais)[0]
-    if resultado == diagnostico[int]:
-      return jsonify ({"message":(diagnostico[int])})
+    print(dadosFinais)
+    retorno = int(resultado)
+    return jsonify ({"message":(dicionario_doencas[retorno])})
+
+app.run()
